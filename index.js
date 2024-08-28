c794d531cb77720ca3ecf43c14e0ef759b2e1ff3
Seitek2002 <seitekbegaliev@gmail.com>
2024-08-29 00:12:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+new Swiper('.mySwiper', {
+  spaceBetween: 30,
+  effect: 'fade',
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
