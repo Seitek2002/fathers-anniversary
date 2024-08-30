68cdc779952d620166913e2518d0be49805e1881
Seitek2002 <seitekbegaliev@gmail.com>
2024-08-30 16:51:02 +0600

    some tests

--- functions/send-message.js
+++ functions/send-message.js
@@ -1,22 +1,27 @@
-exports.handler = async function(event, context) {
-    try {
-        // Ваша логика
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*', // Разрешение запросов с любых источников
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Methods': 'POST'
-            },
-            body: JSON.stringify({ message: 'Сообщение отправлено!' })
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            },
-            body: JSON.stringify({ error: 'Ошибка при отправке сообщения.' })
-        };
-    }
+const axios = require('axios');
+
+exports.handler = async function (event, context) {
+  const { text1, text2, check_state } = JSON.parse(event.body);
+  const token = 'В7182997455:AAE3x3tfHXi3hi3oLK4xCcUDaoCThUY2Cac';
+  const chatId = '1025323599';
+
+  try {
+    const response = await axios.post(
+      `https://api.telegram.org/bot${token}/sendMessage`,
+      {
+        chat_id: chatId,
+        text: `Text1: ${text1}\nText2: ${text2}\nCheckbox: ${check_state}`,
+      }
+    );
+    return {
+      statusCode: 200,
+      body: 'Сообщение отправлено!',
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: 'Ошибка при отправке сообщения.',
+    };
+  }
 };
